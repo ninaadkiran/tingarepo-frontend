bf7a1e57585c095b45e7aff3aba9b36e63af0c2a
EthanH06 <skne8816@gmail.com>
2024-01-16 20:02:38 -0800

    Began Making Flappy Bird

---
+++ FlappyBird/Scripts/flappybird.js
@@ -0,0 +1,11 @@
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let econtext;
+
+window.onload = function (){
+    board = document.GetElementById("board");
+    board.height = board.Height;
+    board.width = board.Width;
+    context = board.getContext("2d");
+}
