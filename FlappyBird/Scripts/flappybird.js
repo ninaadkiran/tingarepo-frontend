d3abb4e7a86a74e34a9dd1253ee4d2b349fb0d57
EthanH06 <skne8816@gmail.com>
2024-01-23 09:35:20 -0800

    shmall tingies

--- FlappyBird/Scripts/flappybird.js
+++ FlappyBird/Scripts/flappybird.js
@@ -3,9 +3,31 @@ let boardWidth = 360;
 let boardHeight = 640;
 let econtext;
 
+let birdWidth = 34;
+let birdgHeight = 24;
+let birdX = boardWidth/8
+let birdY = boardHeight/2
+let birdImg;
+
+let bird = {
+    X : birdX,
+    y : birdY,
+    width : birdWidth,
+    height: birdHeight
+}
+
 window.onload = function (){
     board = document.GetElementById("board");
     board.height = board.Height;
     board.width = board.Width;
     context = board.getContext("2d");
+
+    context.fillstyle = "purple"
+    context.fillRect(bird.X, bird.y, bird.width, bird.height);
+
+    birdImg = new Image()
+    birdImg.src = "./flappybird.png"
+    birdImg.onload = function() {
+        context.drawImage(bird.Img, bird.X, bird.y, bird.width, bird.height);
+    }
 }
