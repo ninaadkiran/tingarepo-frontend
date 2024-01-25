bc104ce4162ec2290166242b3c477b67f38ef12e
EthanH06 <skne8816@gmail.com>
2024-01-25 09:44:43 -0800

    added js

--- FlappyBird/Scripts/flappybird.js
+++ FlappyBird/Scripts/flappybird.js
@@ -16,6 +16,18 @@ let bird = {
     height: birdHeight
 }
 
+let pipeArray = [];
+let pipeWidth = 64;
+let pipeHeigt = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+
+let topPipeImg;
+let bottomPipeImg;
+
+let velocityX = -2;
+
+
 window.onload = function (){
     board = document.GetElementById("board");
     board.height = board.Height;
@@ -30,4 +42,42 @@ window.onload = function (){
     birdImg.onload = function() {
         context.drawImage(bird.Img, bird.X, bird.y, bird.width, bird.height);
     }
+
+    topPipeImg = new Image();
+    topPipeImg.src = "./toppipe.png";
+    
+    bottomPipeImg = new Image();
+    bottomPipeImg.src = "./bottomPipeImg.png";
+
+    requestAnimationFrame(update);
+    setInterval(placePipes, 1500);
+
+}
+
+function update() {
+    requestAnimationFrame(update)
+    context.clearRect(0,0, board.width, board.height);
+
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+
+    for (let i = 0; i < pipeArray.length; i++) {
+        let pipe = pipeArray[i];
+        pipe.x += velocityX;
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe,height);
+    }
+}
+
+function placePipes () {
+
+    let topPipe= {
+        img : topPipeImg,
+        x : pipeX,
+        y : pipeY,
+        width : pipeWidth,
+        height : pipeHeight,
+        passed : false
+    }
+    
+    pipeArray.push(topPipe);
+
 }
