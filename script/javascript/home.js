2a7648035c54269fb5e064c41f7c142cb13d0af0
JoshThinh <joshua.thinh@gmail.com>
2024-02-09 10:20:46 -0800

    added home page

---
+++ script/javascript/home.js
@@ -0,0 +1,6 @@
+// JavaScript code for any interactivity
+
+document.getElementById('explore-btn').addEventListener('click', function() {
+    alert('Exploring the galaxy!');
+    // You can add more functionality here, like navigating to another page or triggering animations
+});
